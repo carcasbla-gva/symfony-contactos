d4d6e2ab2a6dd2671941c44b2dd1510f8aadd247
carcasbla-gva <carcasbla@alu.edu.gva.es>
2026-09-21 07:24:54 +0000

    Acabado

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacto;
+use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,8 +21,13 @@ final class PageController extends AbstractController
         ]);
     }
     #[Route('/', name: 'inicio')]
-    public function inicio(): Response
-    {
-        return $this->render("inicio.html.twig");
-    }
+
+public function inicio(ManagerRegistry $doctrine): Response
+{
+    $repositorio = $doctrine->getRepository(Contacto::class);
+    // findAll es un método que se encuentra en el repositorio
+    $contactos = $repositorio->findAll();
+    //Mostramos la plantilla pasándole los contactos
+    return $this->render("inicio.html.twig", ["contactos" => $contactos]);
+}
 }
